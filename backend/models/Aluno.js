e82258dfc304808d66fe248db26257561642094f
arielsonSZ <arielson.sousa@hotmail.com>
2026-03-30 19:57:02 -0300

    Commit inicial

---
+++ backend/models/Aluno.js
@@ -0,0 +1,13 @@
+const mongoose = require('../db');
+
+const AlunoSchema = new mongoose.Schema({
+    nome: String,
+    email: String,
+    telefone: String,
+    curso: String,
+    dataCadastro: {
+        type: Date,
+        default: Date.now
+    }
+});
+module.exports = mongoose.model('Aluno', AlunoSchema);
